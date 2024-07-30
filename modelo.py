5124f79df5047b4b2fb7addc8bef317fcc2310d4
Julián <165721077+Outeyral@users.noreply.github.com>
2024-07-30 19:36:42 -0300

    Update modelo.py

--- modelo.py
+++ modelo.py
@@ -1,37 +1,57 @@
 import sqlite3 as sql
 
-
-def createDB():
-    conn = sql.connect("alumnos.db")
-    conn.commit()
-    conn.close()
-
-def createTable():
-    conn = sql.connect("alumnos.db")
-    cursor = conn.cursor()
-    cursor.execute(
-        """CREATE TABLE alumnos (
-        name text,
-        last_name text,
-        age integer,
-        id integer
-        )"""
-    )
-    conn.commit()
-    conn.close()
+class Model:
     
-def insertRow(name, last_name, age, id):
-    conn = sql.connect("alumnos.db")
-    cursor = conn.cursor()
-    instruction = f"INSERT INTO alumnos VALUES ('{name}', '{last_name}', '{age}', '{id}')"
-    cursor.execute(instruction)
-    conn.commit()
-    conn.close()
+    def createDB(self):
+        conn = sql.connect("alumnos.db")
+        conn.commit()
+        conn.close()
+    
+    def createTable(self):
+        conn = sql.connect("alumnos.db")
+        cursor = conn.cursor()
+        instruction = f"CREATE TABLE IF NOT EXISTS alumnos (
+            name text,
+            last_name text,
+            age integer,
+            id integer
+            )"
+        cursor.execute(instruction)
+        conn.commit()
+        conn.close()
 
-def deleteRow(id):
-    conn = sql.connect("alumnos.db")
-    cursor = conn.cursor()
-    instruction = f"DELETE FROM alumnos WHERE id = '{id}'"
-    cursor.execute(instruction)
-    conn.commit()
-    conn.close()
+    def insertRow(self, name, last_name, age, id):
+        conn = sql.connect("alumnos.db")
+        cursor = conn.cursor()
+        instruction = f"INSERT INTO alumnos VALUES ('{name}', '{last_name}', '{age}', '{id}')"
+        cursor.execute(instruction)
+        conn.commit()
+        conn.close()
+        
+    #No estoy para nada seguro de este método:
+    
+    def rewriteRow(self, name, last_name, age, id):
+        conn = sql.connect("alumnos.db")
+        cursor = conn.cursor()
+        instruction = f"UPDATE alumnos SET name = {name} last_name = {last_name} age = {age} id = {id}"
+        cursor.execute(instruction)
+        conn.commit()
+        conn.close()
+    
+    def deleteRow(self, id):
+        conn = sql.connect("alumnos.db")
+        cursor = conn.cursor()
+        instruction = f"DELETE FROM alumnos WHERE id = '{id}'"
+        cursor.execute(instruction)
+        conn.commit()
+        conn.close()
+
+    def ordered(self):
+        conn = sql.connext("alumnos.db")
+        cursor = conn.cursor()
+        instruction = f"SELECT * FROM alumnos ORDER BY name"
+        cursor.execute(instruction)
+        conn.commit()
+        conn.close()
+
+model = Model()
