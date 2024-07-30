74e8f7aa5b37b684f71f5d511648f052dff72274
Julián <165721077+Outeyral@users.noreply.github.com>
2024-07-30 20:34:01 -0300

    ModelClass.py

--- modelo.py
+++ modelo.py
@@ -10,48 +10,42 @@ class Model:
     def createTable(self):
         conn = sql.connect("alumnos.db")
         cursor = conn.cursor()
-        instruction = f"CREATE TABLE IF NOT EXISTS alumnos (
+        cursor.execute(f"CREATE TABLE IF NOT EXISTS alumnos (
             name text,
             last_name text,
             age integer,
             id integer
-            )"
-        cursor.execute(instruction)
+            )")
         conn.commit()
         conn.close()
 
     def insertRow(self, name, last_name, age, id):
         conn = sql.connect("alumnos.db")
         cursor = conn.cursor()
-        instruction = f"INSERT INTO alumnos VALUES ('{name}', '{last_name}', '{age}', '{id}')"
-        cursor.execute(instruction)
+        cursor.execute(f"INSERT INTO alumnos VALUES ('{self.name}', '{self.last_name}', '{self.age}', '{self.id}')")
         conn.commit()
         conn.close()
-        
-    #No estoy para nada seguro de este método:
     
     def rewriteRow(self, name, last_name, age, id):
         conn = sql.connect("alumnos.db")
         cursor = conn.cursor()
-        instruction = f"UPDATE alumnos SET name = {name} last_name = {last_name} age = {age} id = {id}"
-        cursor.execute(instruction)
+        cursor.execute(f"UPDATE alumnos SET name = '{self.name}', last_name = '{self.last_name}', age = '{self.age}' WHERE id = '{self.id}'")
         conn.commit()
         conn.close()
     
     def deleteRow(self, id):
         conn = sql.connect("alumnos.db")
         cursor = conn.cursor()
-        instruction = f"DELETE FROM alumnos WHERE id = '{id}'"
-        cursor.execute(instruction)
+        cursor.execute(f"DELETE FROM alumnos WHERE id = '{self.id}'")
         conn.commit()
         conn.close()
 
-    def ordered(self):
-        conn = sql.connext("alumnos.db")
+    def orderedBy(self, arg):
+        conn = sql.connect("alumnos.db")
         cursor = conn.cursor()
-        instruction = f"SELECT * FROM alumnos ORDER BY name"
-        cursor.execute(instruction)
+        cursor.execute(f"SELECT * FROM alumnos ORDER BY '{arg}'")
         conn.commit()
         conn.close()
+
 
 model = Model()
